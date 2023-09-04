b7c75ee5c9fa0d12530ac302ecea74a4bddfaa57
anandv2solutions <143715728+anandv2solutions@users.noreply.github.com>
2023-09-04 18:39:51 +0530

    CRUD - Form validation with debouncing

--- src/components/pages/dashboards/personal/PersonalDashboardV1.vue
+++ src/components/pages/dashboards/personal/PersonalDashboardV1.vue
@@ -46,6 +46,8 @@ const isEditing = ref<boolean>(false)
 
 const rowData = ref<IRowDataType[]>()
 
+const isButtonDisabled = ref<boolean>(true)
+
 const newRowData: IRowDataType = reactive({
   product_name: '',
   original_price: 0,
@@ -58,6 +60,32 @@ const openForm = () => {
   isEditing.value = !isEditing.value
   isDataLoaded.value = !isDataLoaded.value
 }
+
+const buttonDisabledCheck = () => {
+  newRowData.product_name.length > 5 &&
+  newRowData.original_price > 0 &&
+  newRowData.sale_price > 0 &&
+  newRowData.product_type > 0 &&
+  newRowData.description.length > 8
+    ? (isButtonDisabled.value = false)
+    : (isButtonDisabled.value = true)
+}
+
+function debounceFunc(func, delay) {
+  let timeoutId
+
+  return function (...args) {
+    clearTimeout(timeoutId)
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
+
+// Create a debounced version of your function
+const betterButtonDisabledCheck = debounceFunc(buttonDisabledCheck, 300) // Adjust the delay as needed (e.g., 300 milliseconds)
+
 const addData = async () => {
   try {
     const postPayload = {
@@ -81,6 +109,7 @@ const addData = async () => {
     console.error('Error sending POST request:', error)
   }
 }
+
 const getRowData = () => {
   axios.defaults.headers['api_key'] = 'Z9Q7WKEY7ORGBUFGN3EG1QS5Y7FG8DU29GHKKSZH'
   axios.get('https://lobster-app-ddwng.ondigitalocean.app/product/list').then((res) => {
@@ -104,52 +133,67 @@ onMounted(() => {
   />
   <button v-if="isDataLoaded" @click="openForm">Add new item</button>
   <form v-if="isEditing" @submit.prevent="addData">
-    <div>
-      <label>Please insert your product name</label
-      ><input
-        v-model="newRowData.product_name"
-        type="text"
-        required
-        placeholder="Please enter title.."
-      />
-    </div>
-    <div>
-      <label>Please insert your product original price</label
-      ><input
-        v-model="newRowData.original_price"
-        type="number"
-        required
-        placeholder="Please enter price.."
-      />
-    </div>
-    <div>
-      <label>Please insert your product sale price</label
-      ><input
-        v-model="newRowData.sale_price"
-        type="number"
-        required
-        placeholder="Please enter sale price.."
-      />
-    </div>
-    <div>
-      <label>How many product you want</label
-      ><input
-        v-model="newRowData.product_type"
-        type="number"
-        required
-        placeholder="Please enter product count.."
-      />
-    </div>
-    <div>
-      <label>Please insert some description</label
-      ><input
-        v-model="newRowData.description"
-        type="text"
-        required
-        placeholder="Please enter some description.."
-      />
-    </div>
-    <button type="submit">Add item</button>
+    <VField>
+      <VLabel>Please insert your product name</VLabel>
+      <VControl>
+        <VInput
+          v-model="newRowData.product_name"
+          type="text"
+          required
+          placeholder="Please enter title.."
+          @blur="betterButtonDisabledCheck"
+        />
+      </VControl>
+    </VField>
+    <VField>
+      <VLabel>Please insert your product original price</VLabel>
+      <VControl>
+        <VInput
+          v-model="newRowData.original_price"
+          type="number"
+          required
+          placeholder="Please enter price.."
+          @change="betterButtonDisabledCheck"
+        />
+      </VControl>
+    </VField>
+    <VField>
+      <VLabel>Please insert your product sale price</VLabel>
+      <VControl>
+        <VInput
+          v-model="newRowData.sale_price"
+          type="number"
+          required
+          placeholder="Please enter sale price.."
+          @change="betterButtonDisabledCheck"
+        />
+      </VControl>
+    </VField>
+    <VField>
+      <VLabel>How many product you want</VLabel>
+      <VControl>
+        <VInput
+          v-model="newRowData.product_type"
+          type="number"
+          required
+          placeholder="Please enter product count.."
+          @change="betterButtonDisabledCheck"
+        />
+      </VControl>
+    </VField>
+    <VField>
+      <VLabel>Please insert some description</VLabel>
+      <VControl>
+        <VInput
+          v-model="newRowData.description"
+          type="text"
+          required
+          placeholder="Please enter some description.."
+          @blur="betterButtonDisabledCheck"
+        />
+      </VControl>
+    </VField>
+    <VButton :disabled="isButtonDisabled" color="primary" type="submit">Add item</VButton>
   </form>
 </template>
 
@@ -172,5 +216,13 @@ input {
   margin-top: 7px;
   margin-bottom: 3px;
   margin-left: 12px;
+}
+
+.field {
+  display: flex;
+
+  label {
+    margin-top: 16px;
+  }
 }
 </style>
